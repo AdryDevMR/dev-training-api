1b05e1b35ba800da8982fd923013a83c2f33d661
Adry Mateo Ramon <adrydevmateo@gmail.com>
2025-08-22 10:22:37 -0400

    1

--- app/api/v1/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-# Import all schemas to make them available when importing from schemas
-from app.api.v1.schemas.base import *
-from app.api.v1.schemas.users import *
-from app.api.v1.schemas.tasks import *
--- app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# This file makes the directory a Python package
-from .base import Base  # noqa
-from .user import User  # noqa
-from .task import Task  # noqa
